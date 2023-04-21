9c578c0b38f518f899389c542e07b2f2dc5e5c12
shi0n0 <shiono.yui1080@gmail.com>
2023-04-21 21:50:14 +0900

    Add task display and creation features

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,18 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-end
+
+  def new
+    @task = Task.new
+  end
+
+  def create 
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
+  end
