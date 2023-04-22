e92f6fdc613fcee08abd1a2c9ead14ee4f5ace26
shi0n0 <shiono.yui1080@gmail.com>
2023-04-22 13:51:28 +0900

    Update and edit functions added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,16 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   private
     def task_params
       params.require(:task).permit(:title)
